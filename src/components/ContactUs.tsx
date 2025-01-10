2e688418c91b5f4eed52e2338b08ee32dfcd2c4c
Karani <karnisinghsf@gmail.com>
2025-01-10 14:01:16 +0530

    fix

--- src/components/ContactUs.tsx
+++ src/components/ContactUs.tsx
@@ -5,21 +5,21 @@ const ContactUs = () => {
     <>
       <div
       id="contact-us"
-        className="flex flex-col md:flex-row  w-full h-auto md:h-[80vh] justify-between  bg-white "
+        className="flex flex-col md:flex-row  w-full h-auto md:h-[90vh] justify-between  bg-white "
       >
         {/* Left Section */}
         <div className="w-full md:w-[50%] pt-10  md:pt-20 bg-blue-700 pb-10 flex flex-col items-center text-center">
           <a href="">
             <img
-              className="w-[150px] md:w-[200px] mb-5"
+              className="w-[350px] md:w-[200px] mb-5"
               src="jdclogo.png"
               alt="logoimg"
             />
           </a>
-          <h4 className="text-yellow-400 text-center md:text-left">
+          <h4 className="text-yellow-400 text-2xl text-center pt-20 md:text-left">
             Have a project in mind?
           </h4>
-          <h1 className="text-4xl md:text-7xl text-white text-center md:text-left mt-4">
+          <h1 className="text-4xl md:text-7xl text-white pt-20 text-center md:text-left mt-4">
             Let's talk!
           </h1>
           {/* <div className="flex items-center mt-10">
@@ -33,7 +33,7 @@ const ContactUs = () => {
 
         {/* Right Section */}
         <div className="w-full md:w-[50%] mt-20 md:p-10">
-          <form className="space-y-6 pl-5 pr-5">
+          <form className="space-y-6 pl-10 pr-10">
             <div className="flex flex-col md:flex-row items-center justify-between">
               <label className="mb-2 md:mb-0 md:mr-4 w-full md:w-[30%]" htmlFor="">
                 Enter your name
@@ -84,7 +84,7 @@ const ContactUs = () => {
 
             <div className="flex justify-center mt-5">
               <button
-                className="bg-indigo-500 text-white font-bold py-2 px-8 rounded-lg hover:bg-indigo-600 transform hover:scale-110 transition-transform duration-300"
+                className="bg-indigo-500 mb-5 text-white font-bold py-2 px-8 rounded-lg hover:bg-indigo-600 transform hover:scale-110 transition-transform duration-300"
                 type="submit"
               >
                 Submit
